1455e0a5ae27215771ca9ecc8e510f74bac9dec2
gabriel belo <gabrielrbelo@gmail.com>
2020-10-14 19:02:46 -0300

    album new and create actions and routes created; new and created policies updated; album new view created (in progress);

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,13 +1,37 @@
 class AlbumsController < ApplicationController
-  before_action :set_artist, only: :index
+  before_action :set_artist, only: [:index, :new, :create]
 
   def index
     @albums = policy_scope(@artist.albums)
   end
 
+  def new
+    @artists = Artist.all
+    @album = Album.new
+    @album.artist = @artist
+    authorize @album
+  end
+
+  def create
+    @album = Album.new(album_params)
+    @album.artist = @artist
+    authorize @album
+    if @album.save!
+      redirect_to artist_albums_path(@artist), notice: 'Your album was successfully created'
+    else
+      render :new
+    end
+  end
+
   private
+
+
 
   def set_artist
     @artist = Artist.find(params[:artist_id])
+  end
+
+  def album_params
+    params.require(:album).permit(:name, :year)
   end
 end
