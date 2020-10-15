a373216571147a5eab5035b64f2e2367b551fe6e
gabriel belo <gabrielrbelo@gmail.com>
2020-10-15 11:01:40 -0300

    after login, user is redirected to artists index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'user', to: "artists#index", as: :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artists, only: :index do
     resources :albums, only: [:index, :edit, :update, :destroy]
