2c706ffa7563dba02b9b725fcbd28e951e1c0c22
gabriel belo <gabrielrbelo@gmail.com>
2020-10-15 12:28:53 -0300

    validations added to artist and album models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,9 @@
 class Album < ApplicationRecord
   belongs_to :artist
+
+  validates :name, presence: true
+  validates :year, presence: true,
+                   format: { with: /\A(19|[2-9][0-9])\d{2}\z/,
+                             message: 'Needs to follow the format: yyyy' }
+  validates :artist, presence: true
 end
